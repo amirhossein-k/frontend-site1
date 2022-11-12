574e048a808489045ad559a77e002a8f1502408b
amirhossein-k <amirhossein.k787878@gmail.com>
2022-11-12 10:14:11 -0800

    1

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
+import { composeWithDevTools } from "redux-devtools/extension";
 import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 const reducer = combineReducers({
   // this will contain our reducers
