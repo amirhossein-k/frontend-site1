41973a3d9fa4479feb0a0d3c600ef46588aeff46
amirhossein karimi <haamir3030@gmail.com>
2022-11-13 19:17:09 +0000

    Create MainScreen.js

---
+++ src/components/MainScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+const MainScreen = () => {
+  return (
+    <div className="mainback">
+      <Container>
+          <Row>
+              <div className='page'>
+          </Row>
+      </Container>
+    </div>
+  );
+};
+export default MainScreen;
