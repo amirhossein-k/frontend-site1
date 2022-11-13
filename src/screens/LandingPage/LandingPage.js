fb72c487c0823ab3a112bb822c402af2aeee4cf3
amirhossein karimi <haamir3030@gmail.com>
2022-11-13 17:45:28 +0000

    Update 3 files

--- src/screens/LandingPage/LandingPage.js
+++ src/screens/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
-import  "./Landing.css";
+import { Button, Container, Row } from "react-bootstrap";
+import "./Landing.css";
 
 const LandingPage = () => {
   return (
@@ -11,7 +11,7 @@ const LandingPage = () => {
         alignItems: "center",
         backgroundImage: "url('https://www.upsara.com/do.php?img=26684')",
         backgroundSize: "cover",
-        backgroundPosition: "center"
+        backgroundPosition: "center",
       }}
     >
       <Container>
@@ -20,6 +20,22 @@ const LandingPage = () => {
             <div>
               <h1 className="text-white">iii To My Site</h1>
               <p className="text-white"> This is Best Site ForEver</p>
+            </div>
+            <div className="buttonContainer">
+              <a href="/login">
+                <Button size="lg" className="landingbutton">
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button
+                  size="lg"
+                  className="landingbutton"
+                  variant="outline-primary"
+                >
+                  Sign Up
+                </Button>
+              </a>
             </div>
           </div>
         </Row>
