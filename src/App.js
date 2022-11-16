0d1a5232eee0d7088c0788445511c02662c43f35
amirhossein karimi <haamir3030@gmail.com>
2022-11-16 10:02:42 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
-import "./App.css";
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MyNotes from "./screens/MyNotes/MyNotes";
-import RegisterScreen from " ./RegisterScreen/RegisterScreen";
-import LoginScreen from " ./screens/LoginScreen/LoginScreen";
+import RegisterScreen from " ./screens/RegisterScreen/RegisterScreen";
+// import LoginScreen from " ./screens/LoginScreen/LoginScreen";
+import "./App.css";
 
 const App = () => {
   useEffect(() => {
@@ -25,7 +25,7 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" exact element={<LandingPage />} />
-          <Route path="/login" exact element={<LoginScreen />} />
+          {/* <Route path="/login" exact element={<LoginScreen />} /> */}
           <Route path="/register" exact element={<RegisterScreen />} />
           <Route path="/mynotes" element={<MyNotes />} />
         </Routes>
