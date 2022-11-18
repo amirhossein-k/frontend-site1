9a1c3d7c0dc3ad92ab7422fca137d49b7276ec77
amirhossein karimi <haamir3030@gmail.com>
2022-11-18 16:43:32 +0000

    Update 3 files

--- src/screens/SingleNote/SingleNote.js
+++ src/screens/SingleNote/SingleNote.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {useDispatch,useSelector} from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import MainScreen from "../../components/MainScreen";
 import ReactMarkdown from "react-markdown";
@@ -7,63 +7,77 @@ import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
 import { createNoteAction } from "../../actions/notesActions";
 import { Button, Card, Form } from "react-bootstrap";
-import {  updateNoteAction } from "../../actions/notesActions";
+import { updateNoteAction } from "../../actions/notesActions";
 import axios from "axios";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { ObjectId } from "mongodb";
 
-const SingleNote = () =>{
-    let navigate = useNavigate()
-     const { id } = useParams()
-     
-    //////////////////
-    const [title,setTitle] = useState('')
-    const [content,setContent] = useState('')
-    const [category,setCategory] = useState('')
-    const [date,setDate] = useState('')
-    /////////////////////
-    const dispatch = useDispatch()
-    const noteUpdate = useSelector(state=>state.noteUpdate)
-    const {loading,error} =noteUpdate
-    ////
-    console.log(typeof(id),typeof(Number(id)))
-    ///////////////////////
-    useEffect(()=>{
-        const fetching = async () =>{
-            const {data} = await axios.get(`https://n07siw-8000.preview.csb.app/api/notes/${id}}`)
-            
-            setTitle(data.title)
-            setContent(data.contnet)
-            setCategory(data.category)
-            setDate(data.updatedAt)
-        }
-        fetching()
-    },[date,id])
-    ///////////
-    const updatehandler = (e) =>{
-        e.preventDefault()
-         dispatch(updateNoteAction(id, title, content, category));
+const SingleNote = () => {
+  let navigate = useNavigate();
+  const { id } = useParams();
+  ////////////////
+  function strRemoveAll(s, r) {
+    return s.replaceAll(r, "");
+  }
+  const number_id = strRemoveAll(id, "'");
+  console.log(number_id);
+  const objectId = new ObjectId("your-object-id-as-string");
+  //////////////////
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [category, setCategory] = useState("");
+  const [date, setDate] = useState("");
+  /////////////////////
+  const dispatch = useDispatch();
+  const noteUpdate = useSelector((state) => state.noteUpdate);
+  const { loading, error } = noteUpdate;
+  ////
+
+  ///////////////////////
+  useEffect(() => {
+    const fetching = async () => {
+      const url = `https://n07siw-8000.preview.csb.app/api/notes/${number_id}}`;
+      console.log(url);
+      const { data } = await axios.get(url);
+
+      setTitle(data.title);
+      setContent(data.contnet);
+      setCategory(data.category);
+      setDate(data.updatedAt);
+    };
+    fetching();
+  }, [date, number_id]);
+  ///////////
+  const updatehandler = (e) => {
+    e.preventDefault();
+    dispatch(updateNoteAction(number_id, title, content, category));
     if (!title || !content || !category) return;
 
     resetHandler();
-    navigate('/mynotes')
-    }
-     const resetHandler = () => {
+    navigate("/mynotes");
+  };
+  const resetHandler = () => {
     setTitle("");
     setCategory("");
     setContent("");
   };
-    return(
-       <MainScreen title='Edit Note'>
-           <Card>
-               <Card.Header>Edit tour note</Card.Header>
-               <Card.Body>
-                   <Form onSubmit={updatehandler}>
-                        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-                        <Form.Group controlId="title">
-                            <Form.Label>Title</Form.Label>
-                            <Form.Control type='title' placeholder="Enter the Title" value={title} onChange={e=>setTitle(e.target.value)}/>
-                        </Form.Group>
-                        <Form.Group controlId="content">
+  return (
+    <MainScreen title="Edit Note">
+      <Card>
+        <Card.Header>Edit tour note</Card.Header>
+        <Card.Body>
+          <Form onSubmit={updatehandler}>
+            {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="title"
+                placeholder="Enter the Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="content">
               <Form.Label>Content</Form.Label>
               <Form.Control
                 as="textarea"
@@ -90,7 +104,7 @@ const SingleNote = () =>{
                 onChange={(e) => setCategory(e.target.value)}
               />
             </Form.Group>
-             {loading && <Loading size={50} />}
+            {loading && <Loading size={50} />}
             <Button variant="primary" type="submit">
               Update Note
             </Button>
@@ -101,14 +115,13 @@ const SingleNote = () =>{
             >
               Delete Note
             </Button> */}
-
-                   </Form>
-               </Card.Body>
-               <Card.Footer className="text-muted">
+          </Form>
+        </Card.Body>
+        <Card.Footer className="text-muted">
           Updated on - {date.substring(0, 10)}
         </Card.Footer>
-           </Card>
-       </MainScreen>
-    )
-}
+      </Card>
+    </MainScreen>
+  );
+};
 export default SingleNote;
